3f50bc13e5496768fe33e95dede73d1e344dbccd
einargudnig <einargudnig@gmail.com>
2020-03-10 23:31:48 +0000

    fix?

--- src/components/Drink/drink.js
+++ src/components/Drink/drink.js
@@ -4,35 +4,36 @@ import './drink.scss'
 
 function DrinkBeer() {
     
+    const isBrowser = () => typeof window !== "undefined"
     /** Stats for beer
      * initialites beer counter as 0 and stores value in local storage.
      * use hooks to change countBeer with setCountBeer
      * have two buttons, increment and decrement
      */
-    const initialCountBeer = () => Number (window.localStorage.getItem('countBeer') || 0)
+    const initialCountBeer = () => Number (isBrowser() && window.localStorage.getItem('countBeer') || 0)
     const [countBeer, setCountBeer] = useState(initialCountBeer)
     const incrementBeer = () => setCountBeer(countBeer + 1)
     const decrementBeer = () => setCountBeer(countBeer - 1)
     useEffect(() => {
-        window.localStorage.setItem('countBeer', countBeer)
+        isBrowser() && window.localStorage.setItem('countBeer', countBeer)
     }, [countBeer])
 
     /** stats for shots */
-    const initialCountShots = () => Number (window.localStorage.getItem('countShots') || 0)
+    const initialCountShots = () => Number (isBrowser() && window.localStorage.getItem('countShots') || 0)
     const [countShots, setCountShots] = useState(initialCountShots)
     const incrementShots = () => setCountShots(countShots + 1)
     const decrementShots = () => setCountShots(countShots - 1)
     useEffect(() => {
-        window.localStorage.setItem('countShots', countShots)
+        isBrowser() && window.localStorage.setItem('countShots', countShots)
     }, [countShots])
 
     /** Stats for Drinks/Wine */
-    const initialCountDrinks = () => Number (window.localStorage.getItem('countDrinks') || 0)
+    const initialCountDrinks = () => Number (isBrowser() && window.localStorage.getItem('countDrinks') || 0)
     const [countDrinks, setCountDrinks] = useState(initialCountDrinks)
     const incrementDrinks = () => setCountDrinks(countDrinks + 1)
     const decrementDrinks = () => setCountDrinks(countDrinks - 1)
     useEffect(() => {
-        window.localStorage.setItem('countDrinks', countDrinks)
+        isBrowser() && window.localStorage.setItem('countDrinks', countDrinks)
     }, [countDrinks])
 
     
