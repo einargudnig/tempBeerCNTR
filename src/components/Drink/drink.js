a9fd854a925fea919dd349d4c98179741a5d02dc
einargudnig <einargudnig@gmail.com>
2020-03-10 23:27:48 +0000

    !localStorage fix

--- src/components/Drink/drink.js
+++ src/components/Drink/drink.js
@@ -4,37 +4,35 @@ import './drink.scss'
 
 function DrinkBeer() {
     
-    const windowGlobal = typeof window !== 'undefined' && window;
-
     /** Stats for beer
      * initialites beer counter as 0 and stores value in local storage.
      * use hooks to change countBeer with setCountBeer
      * have two buttons, increment and decrement
      */
-    const initialCountBeer = () => Number (windowGlobal.localStorage.getItem('countBeer') || 0)
+    const initialCountBeer = () => Number (window.localStorage.getItem('countBeer') || 0)
     const [countBeer, setCountBeer] = useState(initialCountBeer)
     const incrementBeer = () => setCountBeer(countBeer + 1)
     const decrementBeer = () => setCountBeer(countBeer - 1)
     useEffect(() => {
-        windowGlobal.localStorage.setItem('countBeer', countBeer)
+        window.localStorage.setItem('countBeer', countBeer)
     }, [countBeer])
 
     /** stats for shots */
-    const initialCountShots = () => Number (windowGlobal.localStorage.getItem('countShots') || 0)
+    const initialCountShots = () => Number (window.localStorage.getItem('countShots') || 0)
     const [countShots, setCountShots] = useState(initialCountShots)
     const incrementShots = () => setCountShots(countShots + 1)
     const decrementShots = () => setCountShots(countShots - 1)
     useEffect(() => {
-        windowGlobal.localStorage.setItem('countShots', countShots)
+        window.localStorage.setItem('countShots', countShots)
     }, [countShots])
 
     /** Stats for Drinks/Wine */
-    const initialCountDrinks = () => Number (windowGlobal.localStorage.getItem('countDrinks') || 0)
+    const initialCountDrinks = () => Number (window.localStorage.getItem('countDrinks') || 0)
     const [countDrinks, setCountDrinks] = useState(initialCountDrinks)
     const incrementDrinks = () => setCountDrinks(countDrinks + 1)
     const decrementDrinks = () => setCountDrinks(countDrinks - 1)
     useEffect(() => {
-        windowGlobal.localStorage.setItem('countDrinks', countDrinks)
+        window.localStorage.setItem('countDrinks', countDrinks)
     }, [countDrinks])
 
     
